76628012962f0c9cabe9cecc646903f1a843ef2a
Gheorghe Petru Alin <alingheorghe@192-168-0-103.rdsnet.ro>
2018-02-03 14:52:01 +0200

    feat: the main tree of the app

--- src/App.js
+++ src/App.js
@@ -5,9 +5,39 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header>
+          <h1>Pixel Drawing App</h1>
+          <h3>Draw anything in pixels....</h3>
+        </header>
+        <main>
+          <p>Draw the things you like by choosing the size of the board and the color of your pencil.</p>
+          <div>
+            <div>
+              <label>Width:</label>
+              <input type="number" />
+              <label>Height:</label>
+              <input type="number" />
+              <button type="button">Submit board</button>
+            </div>
+            <div>
+              <table>
+                <tr>
+                  <td>dsadsada</td>
+                  <td>dsadsada</td>
+                  <td>dsadsada</td>
+                </tr>
+                <tr>
+                  <td>dsadsada</td>
+                  <td>dsadsada</td>
+                  <td>dsadsada</td>
+                </tr>
+              </table>
+            </div>
+            <div>
+              <button type="button">Save your drawing</button>
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
