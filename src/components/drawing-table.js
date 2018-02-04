f1fefb3b3fa501180767c769636a63dbebc02d79
Gheorghe Petru Alin <alingheorghe@192-168-0-104.rdsnet.ro>
2018-02-04 14:01:01 +0200

    feat: dynamically table algorithm

--- src/components/drawing-table.js
+++ src/components/drawing-table.js
@@ -1,19 +1,29 @@
 import React from 'react';
 
+const x = 30;
+const y = 30;
+
 const DrawingTable = (props) => {
+
+    const array = [];
+    
+    for(let i = 0; i < x; i++) {
+        array.push([]);
+        for(let j=0; j < y; j++){
+            array[i].push(j);
+        }
+    }
+
     return (
         <div>
-            <table>
-                <tr>
-                  <td>dsadsada</td>
-                  <td>dsadsada</td>
-                  <td>dsadsada</td>
-                </tr>
-                <tr>
-                  <td>dsadsada</td>
-                  <td>dsadsada</td>
-                  <td>dsadsada</td>
-                </tr>
+            <table className="Drawing-table">
+                <tbody>
+                {
+                    array.map( (row) => {
+                        return (<tr>{ row.map((column) => { return (<td></td>) }) }</tr>)
+                        })
+                }
+                </tbody>
             </table>
         </div>
     );
